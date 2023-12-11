b7a03d39d131681d577b1073e2f69026a0bb5b63
Luis <luiveldel@alum.us.es>
2023-12-11 18:58:08 +0100

    Reto #5 - Python

---
+++ Retos/Reto #5 - HOLA MUNDO [Fácil]/python/luiveldel.py
@@ -0,0 +1,5 @@
+# Escribe un !Hola Mundo! en todos los lenguajes de programación que puedas.
+# Seguro que hay algún lenguaje que te llama la atención y nunca has utilizado,
+# o quizás quieres dar tus primeros pasos... ¡Pues este es el momento!
+
+print("Hola Mundo!")
